51b5e4728879f3fc4404197ad10036fdb57e7945
mahdi.nasirlo <m.nwordpress1223@gmail.com>
2025-11-16 12:46:18 +0330

    add sub path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  reactStrictMode: false
+  reactStrictMode: false,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: process.env.NEXT_PUBLIC_BASE_PATH,
+        basePath: false,
+        statusCode: 303,
+        permanent: undefined,
+      },
+      {
+        source: "/login",
+        destination: process.env.NEXT_PUBLIC_BASE_PATH + "/login",
+        basePath: false,
+        statusCode: 303,
+        permanent: undefined,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
